ab9fb977669189fc23fbfba01054f9ca372b148c
JOAO ROSIVALDO GOMES <rosivaldo.joao@academico.ifpb.edu.br>
2023-10-25 12:34:45 -0300

    alteracoes

--- NivelHeroi.java
+++ NivelHeroi.java
@@ -14,21 +14,21 @@ public class NivelHeroi{
             System.out.print("Digite o xp do Heroi: ");
             xpHeroi = Integer.parseInt(leitor.nextLine());
             if(xpHeroi < 1000){
-                System.out.println(nomeHeroi + "XP VIDA: "+ xpHeroi + " Ferro");               
+                System.out.println("O Herói de nome " + nomeHeroi + " está no nível de Ferro");               
             }else if(xpHeroi < 2001){
-                 System.out.println(nomeHeroi + "XP VIDA: "+ xpHeroi + " Bronze");
+                 System.out.println("O Herói de nome " + nomeHeroi + " está no nível de Bronze");
             }else if(xpHeroi < 5001){
-                System.out.println(nomeHeroi + "XP VIDA: "+ xpHeroi + " Prata");
+                System.out.println("O Herói de nome " + nomeHeroi + " está no nível de Prata");
             }else if(xpHeroi < 7001){
-                System.out.println(nomeHeroi + "XP VIDA: "+ xpHeroi + " Ouro");
+                System.out.println("O Herói de nome " + nomeHeroi + " está no nível de Ouro");
             }else if(xpHeroi < 8001 ){
-                System.out.println(nomeHeroi + "XP VIDA: "+ xpHeroi + " Platina");
+                System.out.println("O Herói de nome " + nomeHeroi + " está no nível de Platina");
             }else if(xpHeroi < 9001){
-                System.out.println(nomeHeroi + "XP VIDA: "+ xpHeroi + " Ascentente");
+                System.out.println("O Herói de nome " + nomeHeroi + " está no nível de Ascentente");
             }else if(xpHeroi < 10001 ){
-                System.out.println(nomeHeroi + "XP VIDA: "+ xpHeroi + " Imortal");
+                System.out.println("O Herói de nome " + nomeHeroi + " está no nível de Imortal");
             }else {
-                System.out.println(nomeHeroi + "XP VIDA: "+ xpHeroi + " Radiante");
+                System.out.println("O Herói de nome " + nomeHeroi + " está no nível de Radiante");
                 
             }
             System.out.println("Deseja continuar S[Sim]  N[Não]");
